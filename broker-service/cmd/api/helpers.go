ffaf9be8988fb0a97601bdaeb6d411a86fb98603
raphael discky <raph.discky@gmail.com>
2025-02-10 22:58:00 +0700

    feat: update the Broker for a standard JSON format, and connect to our Auth service (#2)
    * feat: update broker for a standard JSON format and connect to our Auth service
    
    * feat: update fe to authenticate through broker
    
    * update file

--- broker-service/cmd/api/helpers.go
+++ broker-service/cmd/api/helpers.go
@@ -13,8 +13,10 @@ type jsonResponse struct {
 	Data    any    `json:"data,omitempty"`
 }
 
+// readJSON tries to read the body of a request and converts it into JSON
 func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
-	maxBytes := 1024 * 1024
+	maxBytes := 1048576 // one megabyte
+
 	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
 
 	dec := json.NewDecoder(r.Body)
@@ -25,12 +27,13 @@ func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) er
 
 	err = dec.Decode(&struct{}{})
 	if err != io.EOF {
-		return errors.New("body must only contain a single JSON value")
+		return errors.New("body must have only a single JSON value")
 	}
 
 	return nil
 }
 
+// writeJSON takes a response status code and arbitrary data and writes a json response to the client
 func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
 	out, err := json.Marshal(data)
 	if err != nil {
@@ -42,10 +45,10 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header
 			w.Header()[key] = value
 		}
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	_, err = w.Write(out)
-
 	if err != nil {
 		return err
 	}
@@ -53,8 +56,11 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header
 	return nil
 }
 
+// errorJSON takes an error, and optionally a response status code, and generates and sends
+// a json error response
 func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) error {
 	statusCode := http.StatusBadRequest
+
 	if len(status) > 0 {
 		statusCode = status[0]
 	}
