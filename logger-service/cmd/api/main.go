938667b030b5446a44b1a1bfcc9878fa1f4511c2
raphaeldiscky <raph.discky@gmail.com>
2025-02-11 14:58:53 +0700

    feat: setup logger data models

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"log"
+	"logger-service/data"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+const (
+	webPort  = "4000"
+	rpcPort  = "4001"
+	grpcPort = "4002"
+	mongoURL = "mongodb://mongo:27017"
+)
+
+var client *mongo.Client
+
+type Config struct {
+	Models data.Models
+}
+
+func main() {
+	// connect to mongo
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+
+	// create a context in order to disconnect
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	// close mongo connection
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// create a client options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	client, err := mongo.Connect(clientOptions)
+	if err != nil {
+		log.Println("Error connecting to mongo", err)
+		return nil, err
+	}
+
+	return client, nil
+}
